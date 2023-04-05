03aaa33e2502c0ce1fd8f419fb4326216d4c0620
Bailey Eccles <bailey123eccles123@gmail.com>
2023-04-05 23:12:34 +0100

    Update platform.py

--- platforms/platform.py
+++ platforms/platform.py
@@ -3,7 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-_PLATFORM = None
+from platforms import local
+_PLATFORM = local.Platform(num_workers=4)
 
 
 def get_platform():
